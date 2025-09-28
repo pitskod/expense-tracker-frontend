3094d31236326096f748fe7d8cb8386d862658c2
Darya Pitsko <darya.pitsko@trustpayments.com>
2025-09-28 17:10:49 +0200

    task 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-import { Logo } from '@/components/Logo';
+import { Logo } from './components/Logo';
+import { Loader } from './components/Loader';
 
 function App() {
   const [count, setCount] = useState(0)
+  let loading = true;
 
   return (
     <>
-     <Logo />
+      <Logo />
+      {loading ? <Loader /> : <p>Данные загружены!</p>}
     </>
   )
 }
